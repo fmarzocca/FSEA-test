de8575fbda6c97c08868c9a7d5a997c4f15a070e
Fabio Marzocca <marzoccafabio@gmail.com>
2017-05-13 13:43:13 +0200

    Adding PDO Exception handling

--- model/UsersGateway.php
+++ model/UsersGateway.php
@@ -11,8 +11,14 @@ class UsersGateway {
         if ( !isset($order) ) {
             $order = "first";
         }
-		$query = $link->prepare("SELECT * FROM ".DB_TABLE." ORDER BY $order ASC");
-		$query->execute();
+        try{
+			$query = $link->prepare("SELECT * FROM ".DB_TABLE." ORDER BY $order ASC");
+			$query->execute();
+		}
+		catch(PDOException $e) {
+			echo 'Exception: '. $e->getMessage();
+        exit;
+    	}	
 		$users = array();
 		$users = $query->fetchAll(PDO::FETCH_OBJ);
         return $users;
@@ -20,39 +26,65 @@ class UsersGateway {
     
     public function selectById($id, $link) {
         /* decrypt password to allow user edit */
-        $query = $link->prepare("SELECT first, last, email,aes_decrypt(password,'secret') as password FROM ".DB_TABLE." WHERE id=:id");
-        $query->bindParam(':id',$id);
-        $query->execute();
+        try{        
+        	$query = $link->prepare("SELECT first, last, email,aes_decrypt(password,'secret') as password FROM ".DB_TABLE." WHERE id=:id");
+        	$query->bindParam(':id',$id);
+        	$query->execute();
+		}
+		catch(PDOException $e) {
+			echo 'Exception: '. $e->getMessage();
+        exit;
+    	}	
+
         return $query->fetch(PDO::FETCH_OBJ);
     }
     
     public function insert( $first, $last, $email, $password, $link ) {
                 
         /* passwords are stored into database with AES encrypting algorythm */
-        $query=$link->prepare("INSERT INTO ".DB_TABLE." (first, last, email, password) VALUES (:first, :last, :email, AES_ENCRYPT(:password,'secret'))");
-        $query->bindParam(':first', $first,PDO::PARAM_STR);
-        $query->bindParam(':last', $last,PDO::PARAM_STR);
-        $query->bindParam(':email', $email,PDO::PARAM_STR);
-        $query->bindParam(':password', $password,PDO::PARAM_STR);        
-        $query->execute();
+		try {
+        	$query=$link->prepare("INSERT INTO ".DB_TABLE." (first, last, email, password) VALUES (:first, :last, :email, AES_ENCRYPT(:password,'secret'))");
+        	$query->bindParam(':first', $first,PDO::PARAM_STR);
+        	$query->bindParam(':last', $last,PDO::PARAM_STR);
+        	$query->bindParam(':email', $email,PDO::PARAM_STR);
+        	$query->bindParam(':password', $password,PDO::PARAM_STR);        
+        	$query->execute();
+        	}
+		catch(PDOException $e) {
+			echo 'Exception: '. $e->getMessage();
+        exit;
+    	}	
  		return $link->lastInsertId();;        
     }
     
     public function delete($id, $link) {
-		$query=$link->prepare("DELETE FROM ".DB_TABLE." WHERE id=:id");
-        $query->bindParam(':id',$id);
-        $query->execute();
+		try {
+			$query=$link->prepare("DELETE FROM ".DB_TABLE." WHERE id=:id");
+        	$query->bindParam(':id',$id);
+        	$query->execute();
+		}
+		catch(PDOException $e) {
+			echo 'Exception: '. $e->getMessage();
+        exit;
+    	}	
+
     }
     
     public function edit($first, $last, $email, $password, $id, $link) {
-    
-        /* passwords are stored into database with AES encrypting algorythm */    
-        $query=$link->prepare("UPDATE ".DB_TABLE." SET first=:first, last=:last, email=:email, password=AES_ENCRYPT(:password,'secret') WHERE id=$id");
-        $query->bindParam(':first', $first,PDO::PARAM_STR);
-        $query->bindParam(':last', $last,PDO::PARAM_STR);
-        $query->bindParam(':email', $email,PDO::PARAM_STR);
-        $query->bindParam(':password', $password,PDO::PARAM_STR);        
-        $query->execute();
+    	try {
+        	/* passwords are stored into database with AES encrypting algorythm */    
+        	$query=$link->prepare("UPDATE ".DB_TABLE." SET first=:first, last=:last, email=:email, password=AES_ENCRYPT(:password,'secret') WHERE id=$id");
+        	$query->bindParam(':first', $first,PDO::PARAM_STR);
+        	$query->bindParam(':last', $last,PDO::PARAM_STR);
+        	$query->bindParam(':email', $email,PDO::PARAM_STR);
+        	$query->bindParam(':password', $password,PDO::PARAM_STR);        
+        	$query->execute();
+		}
+		catch(PDOException $e) {
+			echo 'Exception: '. $e->getMessage();
+        exit;
+    	}	
+
     }
     
 }
