ef2eea2fc7665758491790939582f30f14812586
Fabio Marzocca <marzoccafabio@gmail.com>
2017-05-13 16:32:02 +0200

    ..forgot a docblock!

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * A simple PHP MVC CRUD application
+ * to cover Formstack Software Engineer Assignment
+ *
+ * @package FSEA-test
+ * @author Fabio Marzocca
+ * @link https://github.com/fmarzocca/FSEA-test
+ * @license http://opensource.org/licenses/MIT MIT License
+ * May 2017
+ */
 require_once 'controller/UsersController.php';
 require 'db.inc.php';
 
